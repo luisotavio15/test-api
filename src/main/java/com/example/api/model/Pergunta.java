9c57c6123f1de3dac36fab09fafe427290fab33b
LuisOtavio13 <roberdoogarcia@gmail.com>
2025-12-29 11:31:55 -0300

    fix: eu

--- src/main/java/com/example/api/model/Pergunta.java
+++ src/main/java/com/example/api/model/Pergunta.java
@@ -1,5 +1,6 @@
 package com.example.api.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,9 +29,13 @@ public class Pergunta implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(columnDefinition = "TEXT")
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String description;
+    @Column(columnDefinition = "TEXT")
     private String answer;
+    
     private String senioridade;
     public Long getId() {
         return id;
